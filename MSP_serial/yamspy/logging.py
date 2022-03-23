2b8d71f1030ab1b8f3a6ccd9c148654d2750c823
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-03-24 00:14:42 +0900

    logging file intro

---
+++ MSP_serial/yamspy/logging.py
@@ -0,0 +1,11 @@
+# 파이썬 로깅의 모든 것
+"""
+what is logging?
+- See our program status 
+- Usally We Use print("~~") function But 
+- Sometime want to see more function 
+"""
+import logging
+
+if __name__ == '__main__':
+	logging.error("something wrong!")
